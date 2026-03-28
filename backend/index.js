15beec2c71c8ca63576ffbcebd470aa13bdec295
Ritika <critika163@gmail.com>
2026-03-28 14:00:02 +0530

    final deployment fix

--- backend/index.js
+++ backend/index.js
@@ -221,7 +221,7 @@ app.get("/allPositions", async(req , res) => {
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build","index.html"));
 });
 
 mongoose
